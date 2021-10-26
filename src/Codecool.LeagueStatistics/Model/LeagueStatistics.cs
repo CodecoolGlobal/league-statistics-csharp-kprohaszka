18ed18c0d6475816f4904b7dadf9b26931aa8805
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-10-26 18:00:53 +0200

    feat: Remove unneccesary comments from final code

--- src/Codecool.LeagueStatistics/Model/LeagueStatistics.cs
+++ src/Codecool.LeagueStatistics/Model/LeagueStatistics.cs
@@ -4,16 +4,8 @@ using System.Linq;
 
 namespace Codecool.LeagueStatistics.Model
 {
-    /// <summary>
-    ///     Provides all necessary statistics of played season.
-    /// </summary>
     public static class LeagueStatistics
     {
-        /// <summary>
-        ///     Gets all teams with highests points order, if points are equal next deciding parameter is sum of golas of the team.
-        /// </summary>
-        /// <param name="teams"></param>
-        /// <returns></returns>
         public static IEnumerable<Team> GetAllTeamsSorted(this IEnumerable<Team> teams)
             => from team in teams
                orderby team.CurrentPoints descending, (team.Players.Sum(player => player.Goals)) descending
@@ -23,11 +15,6 @@ namespace Codecool.LeagueStatistics.Model
             => teams.OrderByDescending(team => team.CurrentPoints).ThenByDescending(team => (team.Players.Sum(player => player.Goals)));
 
 
-        /// <summary>
-        ///     Gets all players from each team in one collection.
-        /// </summary>
-        /// <param name="teams"></param>
-        /// <returns></returns>
 
         public static IEnumerable<Player> GetAllPlayers(this IEnumerable<Team> teams)
             => from team in teams
@@ -38,29 +25,17 @@ namespace Codecool.LeagueStatistics.Model
             => teams.SelectMany(team => team.Players).ToList();
 
 
-        /// <summary>
-        ///     Gets team with the longest name
-        /// </summary>
-        /// <param name="teams"></param>
-        /// <returns></returns>
-        ///
+
         public static Team GetTeamWithTheLongestName(this IEnumerable<Team> teams)
             => (from team in teams
                 orderby team.Name.Length descending
                 select team).First();
 
-
         public static Team GetTeamWithTheLongestNameMethodLikeLINQAlternative(this IEnumerable<Team> teams)
             => teams.OrderByDescending(team => team.Name).First();
 
-        /// <summary>
-        ///     Gets top teams with least number of lost matches.
-        ///     If the amount of lost matches is equal, next deciding parameter is team's current points value.
-        /// </summary>
-        /// <param name="teams"></param>
-        /// <param name="teamsNumber">The number of Teams to select.</param>
-        /// <returns>Collection of selected Teams.</returns>
-        ///
+
+
         public static IEnumerable<Team> GetTopTeamsWithLeastLoses(this IEnumerable<Team> teams, int teamsNumber)
             => (from team in teams
                 orderby team.Losts ascending, team.CurrentPoints descending
@@ -69,11 +44,8 @@ namespace Codecool.LeagueStatistics.Model
         public static IEnumerable<Team> GetTopTeamsWithLeastLosesMethodLikeLINQAlternative(this IEnumerable<Team> teams, int teamsNumber)
             => teams.OrderBy(team => team.Losts).ThenByDescending(team => team.CurrentPoints).Take(teamsNumber);
 
-        /// <summary>
-        ///     Gets a player with the biggest goals number from each team.
-        /// </summary>
-        /// <param name="teams"></param>
-        /// <returns></returns>
+
+
         public static IEnumerable<Player> GetTopPlayersFromEachTeam(this IEnumerable<Team> teams)
             => from team in teams
                select (from player in team.Players
@@ -83,27 +55,18 @@ namespace Codecool.LeagueStatistics.Model
         public static IEnumerable<Player> GetTopPlayersFromEachTeamMethodLikeLINQAlternative(this IEnumerable<Team> teams)
             => teams.Select(team => team.Players.OrderByDescending(player => player.Goals).First());
 
-        /// <summary>
-        ///     Returns the division with greatest amount of points.
-        ///     If there is more than one division with the same amount current points, then check the amounts of wins.
-        /// </summary>
-        /// <param name="teams"></param>
-        /// <returns></returns>
+
+
         public static Division GetStrongestDivision(this IEnumerable<Team> teams)
             => (from team in teams
                 orderby team.CurrentPoints descending, team.Wins descending
                 select team.Division).First();
+
         public static Division GetStrongestDivisionMethodLikeLINQAlternative(this IEnumerable<Team> teams)
             => teams.OrderByDescending(team => team.CurrentPoints).ThenByDescending(team => team.Wins).Select(team => team.Division).First();
 
 
 
-
-        /// <summary>
-        ///     Gests all teams, where there are players with no scored goals.
-        /// </summary>
-        /// <param name="teams"></param>
-        /// <returns></returns>
         public static IEnumerable<Team> GetTeamsWithPlayersWithoutGoals(this IEnumerable<Team> teams)
             => from team in teams
                from player in team.Players
@@ -113,13 +76,8 @@ namespace Codecool.LeagueStatistics.Model
         public static IEnumerable<Team> GetTeamsWithPlayersWithoutGoalsMethodLikeLINQAlternative(this IEnumerable<Team> teams)
             => teams.Where(team => team.Players.Any(player => player.Goals == 0));
 
-        /// <summary>
-        /// Gets players with given or higher number of goals scored.
-        /// </summary>
-        /// <param name="teams"></param>
-        /// <param name="goals">The minimal number of golas scored.</param>
-        /// <returns>Collection of Players with given or higher number of goals scored.</returns>
-        ///
+
+
         public static IEnumerable<Player> GetPlayersWithAtLeastXGoals(this IEnumerable<Team> teams, int goals)
             => from team in teams
                from player in team.Players
@@ -130,12 +88,7 @@ namespace Codecool.LeagueStatistics.Model
             => teams.SelectMany(team => team.Players.Where(player => player.Goals >= goals));
 
 
-        /// <summary>
-        ///     Gets the player with the highest skill rate for given Division.
-        /// </summary>
-        /// <param name="teams"></param>
-        /// <param name="division"></param>
-        /// <returns></returns>
+
         public static Player GetMostTalentedPlayerInDivision(this IEnumerable<Team> teams, Division division)
             => (from player in
                (from team in teams
