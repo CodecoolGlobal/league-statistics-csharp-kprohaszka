81fb740cd71e04726b0eae144cd01dc920ed20ae
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-10-26 20:29:39 +0200

    feat: Add Alternative display table printing methods

--- src/Codecool.LeagueStatistics/View/Display.cs
+++ src/Codecool.LeagueStatistics/View/Display.cs
@@ -2,6 +2,7 @@ using Codecool.LeagueStatistics.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using ConsoleTables;
 
 namespace Codecool.LeagueStatistics.View
 {
@@ -11,7 +12,7 @@ namespace Codecool.LeagueStatistics.View
     public static class Display
     {
 
-        public static void displayResultsAfterSingleMatch(Team team1, Team team2)
+        public static void displayResultsAfterSingleMatchAlternative(Team team1, Team team2)
         {
             Console.WriteLine(String.Format("|{0,5}|{1,5}|{2,5}|{3,5}|{4,5}|{5,5}", "Team Name", "Points", "Goals", "Wins", "Draws", "Losses"));
 
@@ -22,7 +23,7 @@ namespace Codecool.LeagueStatistics.View
                     team2.CurrentPoints, team2.Players.Sum(player => player.Goals), team2.Wins, team2.Draws, team2.Losts));
         }
 
-        public static void DisplayLeagueResults(List<Team> league)
+        public static void DisplayLeagueResultsAlternative(List<Team> league)
         {
             Console.WriteLine(String.Format("|{0,5}|{1,5}|{2,5}|{3,5}|{4,5}|{5,5}", "Team Name", "Points", "Goals", "Wins", "Draws", "Losses"));
             foreach (var team in league)
@@ -31,6 +32,32 @@ namespace Codecool.LeagueStatistics.View
                 Console.WriteLine(String.Format("|{0,5}|{1,5}|{2,5}|{3,5}|{4,5}|{5,5}", team.Name,
                     team.CurrentPoints, team.Players.Sum(player => player.Goals), team.Wins, team.Draws, team.Losts));
             }
+        }
+
+        public static void displayResultsAfterSingleMatch(Team team1, Team team2)
+        {
+            Console.Clear();
+            var table = new ConsoleTable("Team Name", "Points", "Goals", "Wins", "Draws", "Losses");
+                table.AddRow(team1.Name, team1.CurrentPoints,
+                    team1.Players.Sum(player => player.Goals), team1.Wins, team1.Draws, team1.Losts)
+                .AddRow(team2.Name, team2.CurrentPoints,
+                team2.Players.Sum(player => player.Goals), team2.Wins, team2.Draws, team2.Losts);
+
+            table.Write();
+            Console.WriteLine();
+        }
+
+        public static void DisplayLeagueResults(List<Team> league)
+        {
+            Console.Clear();
+            var table = new ConsoleTable("Team Name", "Points", "Goals", "Wins", "Draws", "Losses");
+            foreach (var team in league)
+            {
+                table.AddRow(team.Name, team.CurrentPoints,
+                    team.Players.Sum(player => player.Goals), team.Wins, team.Draws, team.Losts);
+            }
+            table.Write();
+            Console.WriteLine();
         }
     }
 }
