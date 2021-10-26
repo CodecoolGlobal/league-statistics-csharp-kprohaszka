f821b80bbbf43b0dcabaf08413c6a048ea083420
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-10-26 19:11:17 +0200

    feat: Correct Mistakes in NamesGenerator

--- src/Codecool.LeagueStatistics/Factory/NamesGenerator.cs
+++ src/Codecool.LeagueStatistics/Factory/NamesGenerator.cs
@@ -7,15 +7,19 @@ namespace Codecool.LeagueStatistics.Facotry
     /// </summary>
     public static class NamesGenerator
     {
+
+        public static string pathPlayerNames = @"C:\My User Files\Codecool\Projects\Advanced Module\Week 2\league-statistics-csharp-kprohaszka\data\PlayerNames.txt";
+        public static string pathTeamNames = @"C:\My User Files\Codecool\Projects\Advanced Module\Week 2\league-statistics-csharp-kprohaszka\data\TeamNames.txt";
+        public static string pathCityNames = @"C:\My User Files\Codecool\Projects\Advanced Module\Week 2\league-statistics-csharp-kprohaszka\data\CityNames.txt";
         public static string GetPlayerName()
         {
-            return File.ReadAllLines("PlayerNames.txt").GetRandomValue();
+            return File.ReadAllLines(pathPlayerNames).GetRandomValue();
         }
 
         public static string GetTeamName()
         {
-            var cities = File.ReadAllLines("CityNames.txt");
-            var names = File.ReadAllLines("TeamNames.txt");
+            var cities = File.ReadAllLines(pathCityNames);
+            var names = File.ReadAllLines(pathTeamNames);
 
             return cities.GetRandomValue() + " " + names.GetRandomValue();
         }
